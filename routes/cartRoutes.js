34323d4bfe3fb483a0a69b495dd90feeca200a34
ikrambenallali <ikrambenallali2003@gmail.com>
2025-10-15 14:14:31 +0100

    get cart

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { addToCart} = require('../controllers/cartController');
+const { addToCart ,getCart} = require('../controllers/cartController');
 const router = express.Router();
 
 // router.get('/:userId', getCart);
 router.post('/', addToCart);
+router.get('/', getCart);
 
 module.exports = router;
