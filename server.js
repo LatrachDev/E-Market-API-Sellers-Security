c1117c36a792d92c3b6da8ea7d1660f5c19b8d97
ayoubjebb2001 <jebbouri1968@gmail.com>
2025-11-05 17:04:45 +0100

    refactor: simplify swagger UI setup by removing explorer option

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ const specs = swaggerJsdoc(options)
 app.use(
   '/api-docs',
   swaggerUi.serve,
-  swaggerUi.setup(specs, { explorer: true })
+  swaggerUi.setup(specs)
 )
 
 app.use(require('./middlewares/notFound'))
